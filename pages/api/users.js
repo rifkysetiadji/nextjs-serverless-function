30d0fc27a4353d6eb65a1bf9ee739aa23c566ee1
rifkysetiadji <rifkywikrama.s@gmail.com>
2021-11-13 11:27:51 +0700

    initial commit

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+const users = [{ id: 1,name:"Rifky" }, { id: 2,name:"Ahmad" }, { id: 3,name:"Messi" }]
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(users)
 }
